ba8f251f130a5182cb59388e01d52f3f6ccfc000
Jck <jck@jck.localdomain>
2021-04-03 22:17:44 -0400

    More tests from docs

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -1,6 +1,22 @@
-<code class="js">// Listening to messages in Context Script
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    console.log(request);
-    // Callback
-    sendResponse({ message: 'Content script has received that message ⚡' })
-})
+let changeColor = document.getElementById("changeColor");
+
+chrome.storage.sync.get("color", ({ color }) => {
+  changeColor.style.backgroundColor = color;
+});
+
+changeColor.addEventListener("click", async () => {
+  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    function: setPageBackgroundColor,
+  });
+});
+
+// The body of this function will be executed as a content script inside the
+// current page
+function setPageBackgroundColor() {
+  chrome.storage.sync.get("color", ({ color }) => {
+    document.body.style.backgroundColor = color;
+  });
+}
